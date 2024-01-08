7aafb1fc4c0a1b5c0847fe6d2c6972eca61c5fd6
khadicad <adil.tacos@gmx.coM>
2024-01-08 17:45:22 +0100

    amelioration graph

--- venv/main.py
+++ venv/main.py
@@ -1,5 +1,11 @@
-from flask import Flask,render_template,send_file
-
+from flask import Flask,render_template,send_file,Response,json,jsonify
+from influxdb_client import InfluxDBClient
+import threading
+# Récupération des données
+url = "http://51.83.36.122:8086"
+token = "q4jqYhdgRHuhGwldILZ2Ek1WzGPhyctQ3UgvOII-bcjEkxqqrIIacgePte33CEjekqsymMqWlXnO0ndRhLx19g=="
+org = "INFO"
+bucket = "IUT_BUCKET"
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,11 +16,72 @@ def home():
 
 @app.route('/graph.html')
 def graph():
-    # Votre logique de traitement peut être ajoutée ici
+
+
+
     # Par exemple, rendre un modèle avec render_template
-    return render_template('graph.html',active_page='Graph')
+    return render_template('graph.html', active_page='Graph')
 
 
+
+
+@app.route('/get_data')
+def get_data():
+    # Create a client
+    client = InfluxDBClient(url=url, token=token, org=org, timeout=20000)
+    # Create a query
+# Create a query
+    # query = 'from(bucket: "IUT_BUCKET")|> range(start: -12h)|> filter(fn: (r) => r["_measurement"] == "°C")|> filter(fn: (r) => r["_field"] == "value")|> filter(fn: (r) => r["domain"] == "sensor")|> filter(fn: (r) => r["entity_id"] == "d351_1_multisensor9_air_temperature")|> aggregateWindow(every: 1h, fn: mean, createEmpty: false)|> yield(name: "mean")'
+    query = 'from(bucket: "IUT_BUCKET")|> range(start: -12h)|> filter(fn: (r) => r["_measurement"] == "ppm")|> filter(fn: (r) => r["_field"] == "value")|> filter(fn: (r) => r["domain"] == "sensor")|> filter(fn: (r) => r["entity_id"] == "d351_1_multisensor9_carbon_dioxide_co2_level")|> difference(nonNegative: true, columns: ["_value"], keepFirst: false)|> filter(fn: (r) => r._value >= 50)|> yield(name: "mean")'
+    
+
+
+
+    # Get the Query API
+    query_api = client.query_api()
+
+    # Execute the query
+    result = query_api.query(org=org, query=query)
+
+    # Process the results
+    # for table in result:
+    #     for record in table.records:
+    #         print(f"Time: {record.get_time()}, Value: {record.get_value()}, Measurement: {record.get_measurement()}")
+
+    # Close the client
+    res = []
+
+    # Rassembler tous les enregistrements de toutes les tables
+    for table in result:
+        for record in table.records:
+            if record.get_measurement() == "°C":
+                res.append({
+                    'time': record.get_time(),
+                    'value': record.get_value(),
+                    'measurement': record.get_measurement(),
+                    'entity':  record['entity_id'],
+                    'nbvaleur': len(table.records)
+
+                })
+            elif record.get_measurement() == "ppm":
+                res.append({
+                    'time': record.get_time(),
+                    'value': record.get_value(),
+                    'measurement': record.get_measurement(),
+                    'entity':  record['entity_id'],
+                    'nbvaleur': len(table.records)
+
+
+                })
+
+
+    res.sort(key=lambda record: record['time'])
+    
+    return Response(generate(res),content_type='text/event-stream')
+
+def generate(res):
+    for record in res:
+        yield f"data: {json.dumps(record)}\n\n"
 @app.route('/tableau.html')
 def tableau():
     # Votre logique de traitement peut être ajoutée ici
